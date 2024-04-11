8de860cbec41d658429d310bedcceadb168cb072
mijakasia <katarzyna.nowak@rsqtechnologies.com>
2024-04-11 11:22:44 +0200

    simple fetch req

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import API_KEY from './request/apiKey'
 import './App.css';
 
-const App = () => <div className="App">Test</div>
+const App = () => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch(`https://api.rawg.io/api/games?key=${API_KEY}`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
+    }, [])
+    
+    return <div className="App">Test</div>
+}
 
 export default App;
